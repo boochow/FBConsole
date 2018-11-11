4957b466c7206b4144da5871543087ae335982f4
boochow <boochowp@gmail.com>
2018-11-11 11:32:14 +0900

    feature: added arrow keys support

--- README.md
+++ README.md
@@ -8,27 +8,16 @@ You can redirect REPL to any framebuffer devices.
 
 The example below duplicates REPL output to SSD1306 OLED connected to ESP32 (SDA=Pin22, SCL=Pin21).
 ```
-def set_fb_console(fb, on=True):
-    global theScreen
-    from fbconsole import FBConsole
-    import os
-    if on:
-        theScreen = FBConsole(fb)
-        os.dupterm(theScreen)
-    else:
-        theScreen = os.dupterm(None)
-
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 i2c = I2C(scl=Pin(21), sda=Pin(22), freq=400000)
 oled = SSD1306_I2C(128, 64, i2c)
 
-set_fb_console(oled)        # redirect REPL output to OLED
-help()                      # and print something
-set_fb_console(oled, False) # then disconnect OLED from REPL
-theScreen.cls()             # and clear OLED screen
+from fbconsole import FBConsole
+import os
+scr = FBConsole(fb)
+os.dupterm(scr)        # redirect REPL output to OLED
+help()                 # and print something
+os.dupterm(None)       # then disconnect OLED from REPL
+scr.cls()              # and clear OLED screen
 ```
-
-## Limitation
-
-Only newlines and backspaces are supported as control charcters. All other control characters such as arrow keys are not supported. (see `_putc()` function)
--- fbconsole.py
+++ fbconsole.py
@@ -22,10 +22,12 @@ class FBConsole(uio.IOBase):
         self.bgcolor = bgcolor
         self.fgcolor = fgcolor
         self.line_height(8)
+        self.cls()
 
     def cls(self):
         self.x = 0
         self.y = 0
+        self.y_end = 0
         self.fb.fill(self.bgcolor)
         try:
             self.fb.show()
@@ -36,36 +38,49 @@ class FBConsole(uio.IOBase):
         self.lineheight = px
         self.w =  self.width // px
         self.h =  self.height // px
-        self.cls()
-        self._draw_cursor()
 
     def _putc(self, c):
         c = chr(c)
         if c == '\n':
-            self.x = 0
             self._newline()
         elif c == '\x08':
             self._backspace()
         elif c >= ' ':
+            self.fb.fill_rect(self.x * 8, self.y * self.lineheight, 8, self.lineheight, self.bgcolor)
             self.fb.text(c, self.x * 8, self.y * self.lineheight, self.fgcolor)
             self.x += 1
             if self.x >= self.w:
                 self._newline()
-                self.x = 0
+
+    def _esq_read_num(self, buf, pos):
+        digit = 1
+        n = 0
+        while buf[pos] != 0x5b:
+            n += digit * (buf[pos] - 0x30)
+            pos -= 1
+            digit *= 10
+        return n
 
     def write(self, buf):
-        self._erase_current()
+        self._draw_cursor(self.bgcolor)
         i = 0
         while i < len(buf):
             c = buf[i]
-            if c == 0x1b: # skip escape sequence
+            if c == 0x1b:
                 i += 1
+                esc = i
                 while chr(buf[i]) in '[;0123456789':
                     i += 1
+                c = buf[i]
+                if c == 0x4b and i == esc + 1:   # ESC [ K
+                    self._clear_cursor_eol()
+                elif c == 0x44:   # ESC [ n D
+                    for _ in range(self._esq_read_num(buf, i - 1)):
+                        self._backspace()
             else:
                 self._putc(c)
             i += 1
-        self._draw_cursor()
+        self._draw_cursor(self.fgcolor)
         try:
             self.fb.show()
         except:
@@ -79,14 +94,13 @@ class FBConsole(uio.IOBase):
             return None
         
     def _newline(self):
+        self.x = 0
         self.y += 1
         if self.y >= self.h:
             self.fb.scroll(0, -8)
             self.fb.fill_rect(0, self.height - self.lineheight, self.width, self.lineheight, self.bgcolor)
             self.y = self.h - 1
-
-    def _erase_current(self):
-        self.fb.fill_rect(self.x * 8, self.y * self.lineheight, 8, self.lineheight, self.bgcolor)
+        self.y_end = self.y
 
     def _backspace(self):
         if self.x == 0:
@@ -95,7 +109,12 @@ class FBConsole(uio.IOBase):
                 self.x = self.w - 1
         else:
             self.x -= 1
-        self.fb.fill_rect(self.x * 8, self.y * self.lineheight, 8, self.lineheight, self.bgcolor)
 
-    def _draw_cursor(self):
-        self.fb.hline(self.x * 8, self.y * self.lineheight + 7, 8, self.fgcolor)
+    def _clear_cursor_eol(self):
+        self.fb.fill_rect(self.x * 8, self.y * self.lineheight, 128, self.lineheight, self.bgcolor)
+        for l in range(self.y + 1, self.y_end):
+            self.fb.fill_rect(0, l * self.lineheight, 128, self.lineheight, self.bgcolor)
+        self.y_end = self.y
+        
+    def _draw_cursor(self, color):
+        self.fb.hline(self.x * 8, self.y * self.lineheight + 7, 8, color)
